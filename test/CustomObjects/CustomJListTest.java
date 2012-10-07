9cf0e020a310135ac90e7b1e1ff642c7eccea5c5
sumit <sumikaul@indiana.edu>
2012-10-06 23:35:17 -0400

    Adding customJListTest

---
+++ test/CustomObjects/CustomJListTest.java
@@ -0,0 +1,88 @@
+package CustomObjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CustomJListTest {
+
+	CustomJListTest customJListTest; 
+	
+	@Before
+	public void setUp() throws Exception {
+		customJListTest=new CustomJListTest();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		customJListTest=null;
+	}
+
+	@Test
+	public void testCustomJList() {
+	
+	}
+
+	@Test
+	public void testMouseClicked() {
+	
+	}
+
+	@Test
+	public void testMouseEntered() {
+	
+	}
+
+	@Test
+	public void testMouseExited() {
+	
+	}
+
+	@Test
+	public void testMousePressed() {
+	
+	}
+
+	@Test
+	public void testMouseReleased() {
+	
+	}
+
+	@Test
+	public void testGetListModel() {
+	
+	}
+
+	@Test
+	public void testSetListModel() {
+	
+	}
+
+	@Test
+	public void testKeyPressed() {
+	
+	}
+
+	@Test
+	public void testKeyReleased() {
+	
+	}
+
+	@Test
+	public void testKeyTyped() {
+	
+	}
+
+	@Test
+	public void testGetSelectedList() {
+	
+	}
+
+	@Test
+	public void testSetSelectedList() {
+	
+	}
+
+}
