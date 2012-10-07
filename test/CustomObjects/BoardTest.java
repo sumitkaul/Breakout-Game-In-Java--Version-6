709ef1b7c60c052acc20f403ceb1ed6b5fd899cf
sumit <sumikaul@indiana.edu>
2012-10-06 23:18:46 -0400

    Adding Test for board

---
+++ test/CustomObjects/BoardTest.java
@@ -0,0 +1,58 @@
+package CustomObjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardTest {
+
+	Board board ;
+	
+	@Before
+	public void setUp() throws Exception {
+		board=new Board();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		board=null;
+	}
+
+	@Test
+	public void testGetBackGroundImagePath() {
+		
+	}
+
+	@Test
+	public void testSetBackGroundImagePath() {
+		
+	}
+
+	@Test
+	public void testBoard() {
+		
+	}
+
+	@Test
+	public void testPaintGraphics() {
+		
+	}
+
+	@Test
+	public void testResize() {
+		
+	}
+
+	@Test
+	public void testGetCurrentLayer() {
+		
+	}
+
+	@Test
+	public void testSetCurrentLayer() {
+		
+	}
+
+}
