5195b59344c021233751adbe02cbcd24043b4781
Ritesh <Ritesh@Ritesh-PC>
2012-10-06 20:02:32 -0400

    added test for customcombobox

---
+++ test/main/CustomComboBoxTest.java
@@ -0,0 +1,36 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import CustomObjects.CustomComboBox;
+
+public class CustomComboBoxTest {
+
+	CustomComboBox customComboBox;
+	ArrayList<String> stringArraylist = new ArrayList<String>();
+	
+	@Before
+	public void setUp() throws Exception {
+		customComboBox = new CustomComboBox(stringArraylist, "title");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		customComboBox = null;
+	}
+
+	@Test
+	public void testTitle() {
+		customComboBox.setTitle("title");
+		String actual = customComboBox.getTitle();
+		String expected = "title";
+		assertTrue(expected.equals(actual));
+	}
+
+}
