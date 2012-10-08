5366355a5c1b509fc5f9644ca3491fa8b634771e
sumit <sumikaul@indiana.edu>
2012-10-07 20:48:00 -0400

    Adding GameMaker Test

---
+++ test/gameMaker/GameMakerTest.java
@@ -0,0 +1,68 @@
+package gameMaker;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameMakerTest {
+
+	
+	GameMaker gamemaker;
+	@Before
+	public void setUp() throws Exception {
+		gamemaker=new GameMaker();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		gamemaker=null;
+	}
+
+	@Test
+	public void testGetInstance() {
+		
+	}
+
+	@Test
+	public void testGameMaker() {
+		
+	}
+
+	@Test
+	public void testCreateArea() {
+		
+	}
+
+	@Test
+	public void testSetResizeState() {
+		
+	}
+
+	@Test
+	public void testGetResizeState() {
+		
+	}
+
+	@Test
+	public void testResize() {
+		
+	}
+
+	@Test
+	public void testGetImageframe() {
+		
+	}
+
+	@Test
+	public void testSetImageframe() {
+		
+	}
+
+	@Test
+	public void testCreateNewObject() {
+		
+	}
+
+}
